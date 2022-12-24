a26561fea4c1d0fbe7be90a3c90a7e9eea0f5dda
Danil Zhikharev <dane99@mail.ru>
2022-12-24 16:05:53 +0500

    Commit_24.12_16:05

--- src/test/java/ru/smpr/web/AuthenticationTests.java
+++ src/test/java/ru/smpr/web/AuthenticationTests.java
@@ -2,49 +2,86 @@ package ru.smpr.web;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+
 
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AuthorizationTest {
+public class AuthenticationTests {
+
+    @BeforeEach
+    void settings() {
+        Configuration.timeout = 10000;
+        Configuration.headless = true;
+        open("http://192.168.30.181/smpr/");
+    }
 
     @Test
-    void successAuthorization() {
-        Configuration.timeout = 10000;
-        open("http://192.168.30.181/smpr/");
+    void successAuthentication() {
         $(By.name("username")).sendKeys("polygon");
         $(By.name("password")).sendKeys("123123");
         $(By.cssSelector("button")).click();
         String actualWeb = $(By.xpath("//span[contains(text(),'Формы визуализации')]")).getText().trim();
         String expectedWeb = "Формы визуализации";
         Assertions.assertEquals(actualWeb, expectedWeb);
+        closeWindow();
     }
 
     @Test
-    void unsuccessfulAuthorization() {
-        open("http://192.168.30.181/smpr/system/login.zul");
+    void successAuthenticationCapsLogin() {
+        $(By.name("username")).sendKeys("POLYGON");
+        $(By.name("password")).sendKeys("123123");
+        $(By.cssSelector("button")).click();
+        String actualWeb = $(By.xpath("//span[contains(text(),'Формы визуализации')]")).getText().trim();
+        String expectedWeb = "Формы визуализации";
+        Assertions.assertEquals(actualWeb, expectedWeb);
+        closeWindow();
+    }
+
+    @Test
+    void unsuccessfulAuthentication() {
         $(By.name("username")).sendKeys("falseLogin");
         $(By.name("password")).sendKeys("falsePassword");
         $(By.cssSelector("button")).click();
         String expectedMessage = "Ваша попытка входа не удалась, попробуйте еще раз.\n" +
                 "Причина: Bad credentials";
-        String actualMessage = $(By.xpath("//span[contains(text(),'Ваша попытка входа не удалась, попробуйте еще раз.')]")).getText().trim();
+        String actualMessage = $(By.xpath("//span[contains(text(),'попытка входа не удалась')]")).getText().trim();
         assertEquals(actualMessage, expectedMessage);
+        closeWindow();
     }
 
     @Test
-    void shouldTestUnsuccessfulAuthorization() {
-        open("http://192.168.30.181/smpr/system/login.zul");
-        $(By.name("username")).sendKeys("falseLogin");
-        $(By.name("password")).sendKeys("falsePassword");
+    void unsuccessfulAuthenticationEmptyLogin() {
+        $(By.name("password")).sendKeys("123123");
         $(By.cssSelector("button")).click();
         String expectedMessage = "Ваша попытка входа не удалась, попробуйте еще раз.\n" +
-                "Причина: Bad credentials";
-        String actualMessage = $(By.xpath("//span[contains(text(),'Ваша попытка входа не удалась, попробуйте еще раз.')]")).getText().trim();
+                "Причина: Empty Username";
+        String actualMessage = $(By.xpath("//span[contains(text(),'попытка входа не удалась')]")).getText().trim();
         assertEquals(actualMessage, expectedMessage);
+        closeWindow();
     }
 
+    @Test
+    void unsuccessfulAuthenticationEmptyPassword() {
+        $(By.name("username")).sendKeys("polygon");
+        $(By.cssSelector("button")).click();
+        String expectedMessage = "Ваша попытка входа не удалась, попробуйте еще раз.\n" +
+                "Причина: Empty Password";
+        String actualMessage = $(By.xpath("//span[contains(text(),'попытка входа не удалась')]")).getText().trim();
+        assertEquals(actualMessage, expectedMessage);
+        closeWindow();
+    }
 
+    @Test
+    void unsuccessfulAuthenticationEmptyForm() {
+        $(By.cssSelector("button")).click();
+        String expectedMessage = "Ваша попытка входа не удалась, попробуйте еще раз.\n" +
+                "Причина: Empty Username";
+        String actualMessage = $(By.xpath("//span[contains(text(),'попытка входа не удалась')]")).getText().trim();
+        assertEquals(actualMessage, expectedMessage);
+        closeWindow();
+    }
 }
